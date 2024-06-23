f303727788d336adea25b0de4df2302ceedfb0c5
kevinchien920819 <22712286+kevinchien920819@users.noreply.github.com>
2024-06-23 12:33:36 +0800

    page update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:flutter_coffee_shop_app/models/coffee_shop.dart';
-import 'package:flutter_coffee_shop_app/pages/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +9,81 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => CoffeeShop(),
-      builder: (context, child) => const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: HomePage(),
+    return MaterialApp(  // Remove the 'const' keyword
+      home: Scaffold(
+        appBar: AppBar(title: Text('Default Page')),
+        body: MainScreen(),
+      ),
+    );
+  }
+
+}
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              const Expanded(
+                child:Center( 
+                child:Text(
+                  'where you go ?',
+                  style: TextStyle(fontSize: 20),
+                ),
+                ), 
+              ),
+              IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: () {
+
+                },
+              ),
+            ],
+          ),
+        ),
+        //the optionbuttono art presssed
+        OptionButton(label: 'Home', onTap: () {}),
+        OptionButton(label: 'Work place', onTap: () {}),
+        OptionButton(label: 'Saved place', onTap: () {}),
+        Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: FloatingActionButton(
+            onPressed: () { //if the mic are pressed
+
+            },
+            child: Icon(Icons.mic),  // Ensure all children are also const if needed
+            backgroundColor: Colors.white,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class OptionButton extends StatelessWidget {
+  final String label;
+  final VoidCallback onTap;
+
+  const OptionButton({super.key, required this.label, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 8),
+      child: ElevatedButton(
+        onPressed: onTap,
+        child: Text(label),
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size(double.infinity, 50), // specific height
+        ),
       ),
     );
   }
